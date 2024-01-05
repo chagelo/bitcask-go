8cd4af248895c0f719ec0997783f66050592c5df
chagelo <changeto104@gmail.com>
2024-01-05 16:58:49 +0800

    feat: add redis protocol support

--- go.mod
+++ go.mod
@@ -7,13 +7,18 @@ require (
 	github.com/stretchr/testify v1.8.4
 )
 
-require golang.org/x/sys v0.4.0 // indirect
+require (
+	github.com/tidwall/btree v1.1.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gofrs/flock v0.8.1
 	github.com/plar/go-adaptive-radix-tree v1.0.5
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tidwall/redcon v1.6.2
 	go.etcd.io/bbolt v1.3.8
 	golang.org/x/exp v0.0.0-20231226003508-02704c960a9b
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ redis/cmd/server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/tidwall/redcon"
+
+	bitcask "bitcask-go"
+	bitcask_redis "bitcask-go/redis"
+)
+
+const addr = "127.0.0.1:6380"
+
+type BitcaskServer struct {
+	dbs    map[int]*bitcask_redis.RedisDataStructure
+	server *redcon.Server
+	mu     sync.RWMutex
+}
+
+func (svr *BitcaskServer) listen() {
+	log.Println("bitcask server runing, ready to accept connections.")
+	_ = svr.server.ListenAndServe()
+}
+
+func (svr *BitcaskServer) accept(conn redcon.Conn) bool {
+	cli := new(BitcaskClient)
+	svr.mu.Lock()
+	defer svr.mu.Unlock()
+
+	cli.server = svr
+	cli.dbs = svr.dbs[0]
+	conn.SetContext(cli)
+	return true
+}
+
+func (svr *BitcaskServer) close(conn redcon.Conn, err error) {
+	for _, db := range svr.dbs {
+		_ = db.Close()
+	}
+	_ = svr.server.Close()
+}
+
+func main() {
+	// 打开 Redis 数据结构服务
+	redisDataStructure, err := bitcask_redis.NewRedisDataStructure(bitcask.DefaultOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// 初始化 Redis 服务器
+	bitcaskServer := &BitcaskServer{
+		dbs: make(map[int]*bitcask_redis.RedisDataStructure),
+	}
+	bitcaskServer.dbs[0] = redisDataStructure
+
+	// 初始化一个 Redis 服务器
+	bitcaskServer.server = redcon.NewServer(addr, nil, bitcaskServer.accept, bitcaskServer.close)
+	bitcaskServer.listen()
+}
